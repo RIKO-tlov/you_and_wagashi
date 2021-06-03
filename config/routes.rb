c42bdb4f77826783562e6b23ada342753d9d361a
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-03 09:46:40 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+
+  #管理者側
+  namespace :admin do
+    root to: "homes#top"
+    resources :shops, only: [:show, :destroy] do
+      resources :reviews, only: [:index, :destroy]
+    end
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  #ユーザー側
+  scope module: :public do
+    root to: 'homes#top'
+    get "/about" => "homes#about", as: "about"
+    resources :users, only: [:edit, :update, :destroy]
+    get "/users/my_page" => "users#show"
+    get "users/visit" => "users#visit"
+    get "users/wish" => "users#wish"
+    get "users/review" => "users#review"
+    resources :shops do
+      resources :reviews #showアクションいるかどうか検討中
+      resource :visits, only: [:create, :destroy]
+      resource :wishes, only: [:create, :destroy]
+    end
+  end
+
   devise_for :users
-  root to: 'homes#top'
+
 end
