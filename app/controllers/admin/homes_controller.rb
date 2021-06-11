425894b3482a1cdc333e113588bf91246f27bf97
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-11 02:13:04 +0000

    [update]admin-shop delete function

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,4 +3,10 @@ class Admin::HomesController < ApplicationController
   def top
     @shops = Shop.all
   end
+
+  def destroy
+    shop = Shop.find(params[:id])
+    shop.destroy
+    redirect_to request.referer
+  end
 end
