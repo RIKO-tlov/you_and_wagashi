874762b9f2babfebd4b16801320d8e16183cf4fe
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-11 06:35:15 +0000

    [add]genres

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -6,6 +6,6 @@ class Admin::ReviewsController < ApplicationController
 
   def destroy
     Review.find_by(id: params[:id], shop_id:params[:shop_id]).destroy
-    redirect_to root_path
+    redirect_to admin_root_path
   end
 end
