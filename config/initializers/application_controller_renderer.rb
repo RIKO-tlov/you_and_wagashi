cff50cfa42fd97b2deddd3b577bb5bd2cbfbd71d
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-16 10:25:20 +0000

    [change]layouts color

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ed07f16f2cb1359a24f2df6d060a4343527d952a435b5f6910cb230e0e85e5dedb8face4958801528ed9cd59564b92fbd8d797692b9f0a614cc5adf99e0e178a'
