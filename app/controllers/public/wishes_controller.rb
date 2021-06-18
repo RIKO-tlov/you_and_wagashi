52d2815afb5ee614f953cf2ede37439ca5e0fc30
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-18 03:29:37 +0000

    [add]Ajax

--- app/controllers/public/wishes_controller.rb
+++ app/controllers/public/wishes_controller.rb
@@ -1,15 +1,13 @@
 class Public::WishesController < ApplicationController
   def create
-    shop = Shop.find(params[:shop_id])
-    wish = current_user.wishes.new(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    wish = current_user.wishes.new(shop_id: @shop.id)
     wish.save
-    redirect_to request.referer
   end
 
   def destroy
-    shop = Shop.find(params[:shop_id])
-    wish = current_user.wishes.find_by(shop_id: shop.id)
+    @shop = Shop.find(params[:shop_id])
+    wish = current_user.wishes.find_by(shop_id: @shop.id)
     wish.destroy
-    redirect_to request.referer
   end
 end
