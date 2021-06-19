d6a0d41d976ae419198323c08ea53e4ded51a819
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-19 07:01:32 +0000

    [add]google map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_104136) do
+ActiveRecord::Schema.define(version: 2021_06_19_030540) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2021_06_10_104136) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
