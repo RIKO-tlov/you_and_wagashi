8e9d120203824f9a0cc30028d65b8679a1a61eac
RIKO-tlov <bluebyrair@outlook.jp>
2021-06-25 07:55:23 +0000

    [add]responsive

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -20,13 +20,10 @@ class Shop < ApplicationRecord
     Shop.find(Wish.group(:shop_id).order('count(shop_id) desc').limit(3).pluck(:shop_id))
   end
 
-
   validates :name, :kana_name, :postal_code, :address, presence: true
   validates :kana_name, format: { with: /\p{hiragana}/ }
   validates :postal_code, format: { with: /\A\d{7}\z/ }
   validates :description, length: { maximum: 200 }
-  validates :bussiness_start_time, presence: true
-  validates :bussiness_end_time, presence: true
   validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
 
   geocoded_by :address
